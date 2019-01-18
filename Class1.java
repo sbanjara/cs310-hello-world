960472c414bae94794c460043724f49006330b6c
Sabin Banjara <sbanjara@stu.jsu.edu>
2019-01-18 08:52:04 +0545

    Removed f3()

--- Class1.java
+++ Class1.java
@@ -11,10 +11,5 @@ public class Class1 {
     private void f2() {
         // v2.0
     }
-    private void f3() {
-        //hot fix (revised for hotfix)
-    }
-    private void f4() {
-        //New feature(under development; Not finished!)
-    }
+    
 }
